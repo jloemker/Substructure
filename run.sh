73db1f8b3d67b27b0378e06cd7afb272bfef9a75
Johanna Lömker <jloemker@yahoo.de>
2024-08-14 16:47:51 +0200

    setting up O2physics run scripts for extended LP task

---
+++ run.sh
@@ -0,0 +1,15 @@
+export OPTIONS="-b --configuration json://jconfig.json"
+
+o2-analysis-je-jet-deriveddata-producer ${OPTIONS} | o2-analysis-je-jet-finder-data-charged ${OPTIONS} | o2-analysis-timestamp ${OPTIONS} | o2-analysis-event-selection ${OPTIONS} | o2-analysis-trackselection ${OPTIONS} | o2-analysis-multiplicity-table ${OPTIONS} | o2-analysis-centrality-table ${OPTIONS} | o2-analysis-track-propagation ${OPTIONS} | o2-analysis-je-jet-lund-reclustering ${OPTIONS}
+
+
+
+#o2-analysistutorial-correlationv0jet ${OPTIONS} |  o2-analysis-timestamp ${OPTIONS} | o2-analysis-event-selection ${OPTIONS} | o2-analysis-trackselection ${OPTIONS} | o2-analysis-trackextension ${OPTIONS} | o2-analysis-lf-lambdakzerobuilder ${OPTIONS} | o2-analysis-pid-tpc ${OPTIONS} | o2-analysis-pid-tof-base ${OPTIONS} | o2-analysis-pid-tof ${OPTIONS} | o2-analysis-multiplicity-table ${OPTIONS} | o2-analysis-mc-converter ${OPTIONS}
+ #| o2-analysis-lf-lambdakzerobuilder ${OPTIONS}
+
+#o2-analysistutorial-correlationv0jet ${OPTIONS} |  o2-analysis-timestamp ${OPTIONS} | o2-analysis-event-selection ${OPTIONS} | o2-analysis-je-filter ${OPTIONS} | o2-analysis-trackselection ${OPTIONS} | o2-analysis-trackextension ${OPTIONS} | o2-analysis-lf-lambdakzerobuilder ${OPTIONS} | o2-analysis-pid-tpc ${OPTIONS} | o2-analysis-multiplicity-table ${OPTIONS}
+
+#o2-analysis-je-filter ${OPTIONS} | o2-analysis-timestamp ${OPTIONS} | o2-analysis-event-selection ${OPTIONS} | o2-analysis-trackselection ${OPTIONS} | o2-analysis-trackextension ${OPTIONS}| o2-analysis-lf-lambdakzerobuilder ${OPTIONS} | o2-analysis-pid-tpc ${OPTIONS} | o2-analysis-multiplicity-table ${OPTIONS} | o2-analysistutorial-correlationv0jet ${OPTIONS} 
+#| o2-analysis-lf-lambdakzerobuilder ${OPTIONS} gives me [ERROR] SEVERE: Device lambdakzero-builder (197037) had at least one message above severity 4: Unhandled o2::framework::runtime_error reached the top of main of o2-analysis-lf-lambdakzerobuilder, device shutting down. Reason: invalid track covariance
+#| o2-analysis-lf-lambdakzerofinder ${OPTIONS}
+#| o2-analysis-track-propagation ${OPTIONS}
